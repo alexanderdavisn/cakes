301ffa2a72009254695486fa18107e89b16134b7
Alex Davis <alex.davis@cerner.com>
2019-05-18 16:30:14 -0500

    Transfer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  
+  def hello
+    render html: 'Welcome, Alex. We have been expecting you.'
+  end
 end
